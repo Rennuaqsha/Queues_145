02b6d239efc2b8466d2a051e048e72aca23f8b7a
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-12-12 17:38:13 +0800

    initializin the values to indicate the queue is empty

--- CirqularQueues/Program.cs
+++ CirqularQueues/Program.cs
@@ -7,11 +7,17 @@ namespace CircularQueues
         int FRONT, REAR, max = 5;
         int[] queue_array = new int[5];
 
-        public Queues()
+        public Program()
+        {
+            //initializing the values of the variables REAR and FRONT to -1 to indicate that the queue is in initially empty
+            FRONT = -1;
+            REAR = -1;
+        }
+
+        public void intsert(int element)
         {
 
         }
-
 
         static void main(string[] args)
         {
