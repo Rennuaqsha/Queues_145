517309661862c6fbd2cc9fb49da9a50534c01898
Rennu Aqsha <rennu.aqsha.ft21@mail.umy.ac.id>
2022-12-12 17:34:45 +0800

    create variable and create array

--- CirqularQueues/Program.cs
+++ CirqularQueues/Program.cs
@@ -4,6 +4,18 @@ namespace CircularQueues
 {
     class program
     {
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
 
+        public Queues()
+        {
+
+        }
+
+
+        static void main(string[] args)
+        {
+
+        }
     }
 }
